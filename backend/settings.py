8e01165487d304e3c69baffc8e97b132d97b08d3
Raissa Barbosa <raissaglaucie@icloud.com>
2023-08-04 17:22:06 +0200

    testando

--- backend/settings.py
+++ backend/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
@@ -157,6 +157,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # essas paradas aqui eu que adicionei
 MEDIA_ROOT = BASE_DIR / 'staticfiles/media'
 MEDIA_URL = '/staticfiles/media/'
+
+STATIC_URL = '/staticfiles/'
+
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
